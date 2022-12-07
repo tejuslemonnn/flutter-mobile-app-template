fd364570b5340638f756652d0fe4a3de3e8b2945
Bedirhan Sağlam <bedirhansaglam270@gmail.com>
2022-12-07 14:57:06 +0300

    #edited IconExtensions

--- lib/core/extensions/icon_extensions.dart
+++ lib/core/extensions/icon_extensions.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobile_template/core/constants/enums/icon_enums.dart';
+import '../constants/enums/icon_enums.dart';
 import 'package:flutter_mobile_template/core/extensions/string_extensions.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
 extension IconExtensions on IconEnums {
-  Widget get appIcons {
+  Widget get appIcon {
     switch (this) {
       case IconEnums.menu:
-        return SvgPicture.asset("menu".toSvg);
+        return SvgPicture.asset(IconEnums.menu.name.toSvg);
     }
   }
 }
